abfa75d07f87d3315bf8068dbe1f46edec758774
EislerMeinName <126663485+eislerMeinName@users.noreply.github.com>
2023-04-02 12:31:55 +0200

    extended EvalWriter

--- helpclasses/evalwriter.py
+++ helpclasses/evalwriter.py
@@ -17,6 +17,7 @@ from gym_pybullet_drones.envs.single_agent_rl.BaseSingleAgentAviary import Actio
 from gym_pybullet_drones.utils.utils import sync, str2bool
 from gym_pybullet_drones.envs.WindSingleAgentAviary import WindSingleAgentAviary
 from robust_drone_pathfollowing.helpclasses.printout import *
+from robust_drone_pathfollowing.helpclasses.pathplotter import PathPlotter
 import torch
 import matplotlib.pyplot as plt
 from scipy.stats import sem
@@ -26,7 +27,8 @@ class EvalWriter:
 
     def __init__(self, name: str, eval_steps: int, path: str, env: WindSingleAgentAviary, episode_len: int, threshold: float):
         """Initialization of a EvalWriter class.
-            Evaluates sucess rate, the sucess time rate and the average distance half way through the simulation.
+            Evaluates sucess rate, the sucess time rate, the average distance half way through the simulation,
+            the average reward and plots path / goal if it is a signel evaluation.
 
         Parameters
         ----------
@@ -45,24 +47,27 @@ class EvalWriter:
 
         """
 
-        self.goal = None
-        self.last_dist = None
+        if eval_steps == 1:
+            self.pathPlotter = PathPlotter(env.goal)
+        self.mean_reward: float = None
+        self.std_reward: float = None
+        self.goal: List[float] = None
+        self.last_dist: float = None
         self.succeeded: bool = False
         self.total_steps: int = eval_steps
         self.succeded_steps: int = 0
         self.total_time: float = 0
-        self.path = path
-        self.env = env
-        self.threshold = threshold
-        self.episode_len = episode_len
-        self.name = name
-        self.distances = []
-        self.times = []
-        self.STEP = -1
-        self.halfdist = []
+        self.path: str = path
+        self.env: WindSingleAgentAviary = env
+        self.threshold: float = threshold
+        self.episode_len: int = episode_len
+        self.name: str = name
+        self.distances: List[float] = []
+        self.times: List[float] = []
+        self.STEP: int = -1
+        self.halfdist: List[float] = []
 
         self.housekeeping(env)
-
 
     def housekeeping(self, env: WindSingleAgentAviary):
         """Housekeeping function.
@@ -85,14 +90,20 @@ class EvalWriter:
         self.succeeded = False
         self.STEP += 1
 
-
     def update(self):
-        """Updates the writer."""
+        """Updates the writer.
+            Each time the update method is used, it is checked wether it is a single evaluation with a pathplot.
+            If the episode is half way through then the current distance is added to the halfdist List.
+            Then the sucess time is updated."""
 
         dist = self.env.getDist()
         simtime = self.env.getSimTime() + (self.STEP * self.episode_len)
         self.times.append(simtime)
         self.distances.append(dist)
+
+        ### Safe pos if only one episode is evaluated ##################################################################
+        if self.total_steps == 1:
+            self.pathPlotter.addPose(self.env.getPose())
 
         ### Check if half way through ##################################################################################
         if (simtime - (self.STEP * self.episode_len)) >= self.episode_len / 2:
@@ -112,9 +123,19 @@ class EvalWriter:
         self.last_dist = dist
 
     def evaluateModel(self, model):
+        """Method that evaluates a given Model.
+
+        Parameters
+        ----------
+        model:
+            The model that should be evaluated.
+
+        """
+
+        self.mean_reward, self.std_reward = evaluate_policy(model, eval_env, n_eval_episodes=self.total_steps)
         obs = self.env.reset()
         start = time.time()
-        for j in range(1, self.total_steps):
+        for j in range(0, self.total_steps):
             for i in range(self.episode_len * int(self.env.SIM_FREQ / self.env.AGGR_PHY_STEPS)):
                 action, _states = model.predict(obs, deterministic=True)
                 obs, reward, done, info = self.env.step(action)
@@ -129,12 +150,14 @@ class EvalWriter:
         self.env.close()
 
     def close(self):
+        """Method that closes the writer and writes."""
+
         if self.succeeded:
             self.succeded_steps += 1
         self.write()
 
     def write(self):
-        """Prints out the data and writes it to a xlsx file."""
+        """Prints out the data, writes it to a xlsx file and plots it."""
 
         ### Print ######################################################################################################
         rate: str = str((self.succeded_steps / self.total_steps) * 100) + '%'
@@ -142,12 +165,13 @@ class EvalWriter:
         time: str = str(self.total_steps * self.episode_len) + "s"
         tot_time: str = str(self.total_time) + "s"
         if self.total_steps > 1:
-            dist_avg: str = "(" + str(mean(self.halfdist)) + "+-" + str(sem(self.halfdist)) + ")m"
+            dist_avg: str = "(" + str(mean(self.halfdist)) + " +- " + str(sem(self.halfdist)) + ")m"
         else:
             dist_avg: str = str(mean(self.halfdist))
-        msg: str = "Total steps: " + str(self.total_steps) + "\nSucceded steps: " + str(
-            self.succeded_steps) + "\n----------------" + "\nTime: " + str(time) + "\nTime in goal: " + str(
-            tot_time) + "\n----------------" + "\nRate: " + rate + "\nTime Rate: " + time_rate + "\nDistance(T/2): " + dist_avg
+        msg: str = "Total steps: {0}\nSucceded steps: {1}\n----------------\nTime: {2}\nTime in goal: {3}\n----------------\nRate: {4}\nTime Rate: {5}\nDistance(T/2): {6}\nMean reward: {7} +- {8}".format(
+            str(self.total_steps), str(
+                self.succeded_steps), str(time), str(
+                tot_time), rate, time_rate, dist_avg, str(self.mean_reward), str(self.std_reward))
         debug(bcolors.OKBLUE, msg)
 
         ### Write to File ##############################################################################################
@@ -160,7 +184,8 @@ class EvalWriter:
             df2.to_excel(writer, sheet_name='Distances')
 
         ### Plot #######################################################################################################
-        self.distances
+        if self.total_steps == 1:
+            self.pathPlotter.show()
         plt.plot(self.times, self.distances)
         plt.show()
 
@@ -169,7 +194,7 @@ if __name__ == "__main__":
     eval_env = gym.make('WindSingleAgent-aviary-v0', aggregate_phy_steps=5, obs=ObservationType('kin'), act=ActionType('rpm'), mode=0, total_force=0,
                         upper_bound=1, debug=False)
     episode_len = 6
-    eval_steps = 6
+    eval_steps = 1
     eval: EvalWriter = EvalWriter(name='TestWriter', eval_steps=eval_steps, path='test.xlsx', env=eval_env, episode_len=episode_len, threshold=0.05)
     obs = eval_env.reset()
     start = time.time()
