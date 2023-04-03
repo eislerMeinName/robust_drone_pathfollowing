6857413fe8988af55c3ee80f86e8b48ffb33b51a
EislerMeinName <126663485+eislerMeinName@users.noreply.github.com>
2023-04-03 10:01:46 +0200

    cleaned code and comments

--- helpclasses/evalwriter.py
+++ helpclasses/evalwriter.py
@@ -1,20 +1,14 @@
 import time
 from typing import List
 import pandas as pd
-import openpyxl
 import numpy as np
 import gym
 from numpy import mean
-
-from gym_pybullet_drones.envs.single_agent_rl.BaseSingleAgentAviary import ActionType, ObservationType
 from stable_baselines3 import PPO
-from stable_baselines3 import DDPG
 from stable_baselines3.common.evaluation import evaluate_policy
-from gym_pybullet_drones.utils.Logger import Logger
-from gym_pybullet_drones.utils.enums import DroneModel
 from stable_baselines3.common.policies import ActorCriticPolicy as a2cppoMlpPolicy
 from gym_pybullet_drones.envs.single_agent_rl.BaseSingleAgentAviary import ActionType, ObservationType
-from gym_pybullet_drones.utils.utils import sync, str2bool
+from gym_pybullet_drones.utils.utils import sync
 from gym_pybullet_drones.envs.WindSingleAgentAviary import WindSingleAgentAviary
 from robust_drone_pathfollowing.helpclasses.printout import *
 from robust_drone_pathfollowing.helpclasses.pathplotter import PathPlotter
@@ -22,13 +16,15 @@ import torch
 import matplotlib.pyplot as plt
 from scipy.stats import sem
 
-class EvalWriter:
-    """Writer class that writes the evaluated performance data to an xlsx file."""
 
-    def __init__(self, name: str, eval_steps: int, path: str, env: WindSingleAgentAviary, episode_len: int, threshold: float):
+class EvalWriter:
+    """Writer class that writes the evaluated performance data to a xlsx file."""
+
+    def __init__(self, name: str, eval_steps: int, path: str, env: WindSingleAgentAviary,
+                 episode_len: int, threshold: float):
         """Initialization of a EvalWriter class.
-            Evaluates sucess rate, the sucess time rate, the average distance half way through the simulation,
-            the average reward and plots path / goal if it is a signel evaluation.
+            Evaluates success rate, the success time rate, the average distance half way through the simulation,
+            the average reward and plots path / goal if it is a single evaluation.
 
         Parameters
         ----------
@@ -49,13 +45,13 @@ class EvalWriter:
 
         if eval_steps == 1:
             self.pathPlotter = PathPlotter(env.goal)
-        self.mean_reward: float = None
-        self.std_reward: float = None
-        self.goal: List[float] = None
-        self.last_dist: float = None
+        self.mean_reward: float = 0
+        self.std_reward: float = 0
+        self.goal: List[float] = []
+        self.last_dist: float = 0
         self.succeeded: bool = False
         self.total_steps: int = eval_steps
-        self.succeded_steps: int = 0
+        self.succeeded_steps: int = 0
         self.total_time: float = 0
         self.path: str = path
         self.env: WindSingleAgentAviary = env
@@ -83,7 +79,7 @@ class EvalWriter:
 
         self.env = env
         if self.succeeded:
-            self.succeded_steps += 1
+            self.succeeded_steps += 1
 
         self.last_dist = self.env.getDist()
         self.goal = self.env.goal
@@ -92,25 +88,25 @@ class EvalWriter:
 
     def update(self):
         """Updates the writer.
-            Each time the update method is used, it is checked wether it is a single evaluation with a pathplot.
+            Each time the update method is used, it is checked whether it is a single evaluation with a pathplot.
             If the episode is half way through then the current distance is added to the halfdist List.
-            Then the sucess time is updated."""
+            Then the success time is updated."""
 
         dist = self.env.getDist()
         simtime = self.env.getSimTime() + (self.STEP * self.episode_len)
         self.times.append(simtime)
         self.distances.append(dist)
 
-        ### Safe pos if only one episode is evaluated ##################################################################
+        # Safe pos if only one episode is evaluated
         if self.total_steps == 1:
             self.pathPlotter.addPose(self.env.getPose())
 
-        ### Check if half way through ##################################################################################
+        # Check if half way through
         if (simtime - (self.STEP * self.episode_len)) >= self.episode_len / 2:
-            if (self.times[len(self.times) - 2] - (self.STEP * self.episode_len) < self.episode_len / 2):
+            if self.times[len(self.times) - 2] - (self.STEP * self.episode_len) < self.episode_len / 2:
                 self.halfdist.append(dist)
 
-        ### Check if success ###########################################################################################
+        # Check if success
         if not self.succeeded and dist < self.threshold:
             self.succeeded = True
         if self.last_dist > self.threshold > dist:
@@ -153,14 +149,14 @@ class EvalWriter:
         """Method that closes the writer and writes."""
 
         if self.succeeded:
-            self.succeded_steps += 1
+            self.succeeded_steps += 1
         self.write()
 
     def write(self):
         """Prints out the data, writes it to a xlsx file and plots it."""
 
-        ### Print ######################################################################################################
-        rate: str = str((self.succeded_steps / self.total_steps) * 100) + '%'
+        # Print
+        rate: str = str((self.succeeded_steps / self.total_steps) * 100) + '%'
         time_rate: str = str(self.total_time / (self.total_steps * self.episode_len) * 100) + '%'
         time: str = str(self.total_steps * self.episode_len) + "s"
         tot_time: str = str(self.total_time) + "s"
@@ -168,46 +164,46 @@ class EvalWriter:
             dist_avg: str = "(" + str(mean(self.halfdist)) + " +- " + str(sem(self.halfdist)) + ")m"
         else:
             dist_avg: str = str(mean(self.halfdist))
-        msg: str = "Total steps: {0}\nSucceded steps: {1}\n----------------\nTime: {2}\nTime in goal: {3}\n----------------\nRate: {4}\nTime Rate: {5}\nDistance(T/2): {6}\nMean reward: {7} +- {8}".format(
+        msg: str = "Total steps: {0}\nSucceeded steps: {1}\n----------------\nTime: {2}\nTime in goal: {3}\n----------------\nRate: {4}\nTime Rate: {5}\nDistance(T/2): {6}\nMean reward: {7} +- {8}".format(
             str(self.total_steps), str(
-                self.succeded_steps), str(time), str(
+                self.succeeded_steps), str(time), str(
                 tot_time), rate, time_rate, dist_avg, str(self.mean_reward), str(self.std_reward))
         debug(bcolors.OKBLUE, msg)
 
-        ### Write to File ##############################################################################################
-        df = pd.DataFrame([str(self.total_steps), str(self.succeded_steps), rate, "--------", time, tot_time, time_rate , dist_avg],
-                        index=['Total steps', 'Succeded steps', 'Success rate', '--------', 'Time', 'Time in goal', 'Success time rate', 'Distance(T/2)'],
+        # Write to File
+        df = pd.DataFrame([str(self.total_steps), str(self.succeeded_steps), rate, "--------", time, tot_time, time_rate,
+                           dist_avg, self.mean_reward + self.std_reward],
+                        index=['Total steps', 'Succeeded steps', 'Success rate', '--------', 'Time', 'Time in goal',
+                               'Success time rate', 'Distance(T/2)', 'Mean reward'],
                         columns=['Eval Data'])
         df2 = pd.DataFrame(self.distances, self.times)
         with pd.ExcelWriter(self.path) as writer:
             df.to_excel(writer, sheet_name='Data(' + str(self.name) + ')')
             df2.to_excel(writer, sheet_name='Distances')
 
-        ### Plot #######################################################################################################
+        # Plot
         if self.total_steps == 1:
             self.pathPlotter.show()
         plt.plot(self.times, self.distances)
+        plt.xlabel('Time [s]')
+        plt.ylabel('Distance [m]')
+        plt.title('Distance to goal')
         plt.show()
 
 
 if __name__ == "__main__":
-    eval_env = gym.make('WindSingleAgent-aviary-v0', aggregate_phy_steps=5, obs=ObservationType('kin'), act=ActionType('rpm'), mode=0, total_force=0,
+    eval_env = gym.make('WindSingleAgent-aviary-v0', aggregate_phy_steps=5, obs=ObservationType('kin'),
+                        act=ActionType('rpm'), mode=0, total_force=0,
                         upper_bound=1, debug=False)
     episode_len = 6
     eval_steps = 1
-    eval: EvalWriter = EvalWriter(name='TestWriter', eval_steps=eval_steps, path='test.xlsx', env=eval_env, episode_len=episode_len, threshold=0.05)
+    eval: EvalWriter = EvalWriter(name='TestWriter', eval_steps=eval_steps, path='test.xlsx',
+                                  env=eval_env, episode_len=episode_len, threshold=0.05)
     obs = eval_env.reset()
     start = time.time()
     onpolicy_kwargs = dict(activation_fn=torch.nn.ReLU,
                            net_arch=[512, 512, dict(vf=[256, 128], pi=[256, 128])]
                            )
-    model = model = PPO(a2cppoMlpPolicy,
-                        eval_env,
-                        policy_kwargs=onpolicy_kwargs,
-                        tensorboard_log='results/tb/',
-                        verbose=1
-                        )
+    model = PPO(a2cppoMlpPolicy, eval_env, policy_kwargs=onpolicy_kwargs,
+                tensorboard_log='results/tb/', verbose=1)
     eval.evaluateModel(model)
-
-
-
