de0ea3732174346f77c066dbb840c04dc2e202d4
EislerMeinName <126663485+eislerMeinName@users.noreply.github.com>
2023-07-20 14:47:38 +0200

    fixed script

--- eval_scripts/eval_t.py
+++ eval_scripts/eval_t.py
@@ -21,7 +21,7 @@ def run(episodes: int,
         mode: int,
         radius: float,
         algo: List[str],
-        name: List[str]
+        name: List[str],
         ):
     for i, n in enumerate(name):
         debug(bcolors.OKBLUE, '[INFO] Model: ' + n)
@@ -31,33 +31,35 @@ def run(episodes: int,
         elif algo[i] == 'sac':
             model = SAC.load(n)
 
-        t_max: np.array = np.arange(5, 65, 5)
+        t_max: np.array = np.arange(5, 61, 1)
         stds: List[float] = []
         means: np.array = np.array([])
 
         # evaluate for different ts
         for t in t_max:
             debug(bcolors.OKBLUE, '[INFO] episode length: ' + str(t) + 's')
-            eval_env = gym.make('WindSingleAgent-aviary-v0', aggregate_phy_steps=5, obs=ObservationType('kin'),
-                            act=act[i], mode=mode, total_force=0, radius=radius, episode_len=t,
-                            drone_model=DroneModel("hb"))
+            eval_env = gym.make('WindSingleAgent-aviary-v0', aggregate_phy_steps=10, obs=ObservationType('kin'),
+                                act=act[i], mode=mode, total_force=0, radius=radius, episode_len=t,
+                                drone_model=DroneModel("hb"))
 
             eval: EvalWriter = EvalWriter(name='TestWriter', eval_steps=episodes, path='test.xlsx', env=eval_env,
-                                        episode_len=t, threshold=0.05)
+                                          episode_len=t, threshold=0.05)
             mean, std = eval.evaluateModel(model, False)
             stds.append(std)
             means = np.append(means, mean)
 
         # plot
+        clean_name: str = n.replace('..', '').replace('results', '').replace('/', '').replace('.zip', '')
         if mode > 1:
-            plt.errorbar(t_max, means, yerr=stds, fmt='-o')
+            plt.errorbar(t_max, means, yerr=stds, fmt='-o', label=clean_name)
             debug(bcolors.BOLD, '[Result] ' + str(means[0]) + '+-' + str(stds[0]))
             debug(bcolors.BOLD, '[Result] ' + str(means[len(means) - 1]) + '+-' + str(stds[len(stds)] - 1))
         else:
-            plt.plot(t_max, means)
+            plt.plot(t_max, means, label=clean_name)
             debug(bcolors.BOLD, '[Result] ' + str(means[0]))
             debug(bcolors.BOLD, '[Result] ' + str(means[len(means) - 1]))
 
+    plt.legend()
     plt.xlabel('time[s]')
     plt.ylabel('reward')
     plt.show()
