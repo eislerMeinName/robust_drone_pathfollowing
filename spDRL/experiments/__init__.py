cfa86ac2881fb4addbc0110a10cdd7373f828e18
EislerMeinName <126663485+eislerMeinName@users.noreply.github.com>
2023-08-25 20:30:03 +0200

    Delete spDRL directory

--- spDRL/experiments/__init__.py
+++
@@ -1,5 +0,0 @@
-# Do this import to ensure that the Gym environments get registered properly
-from .windsingleagentexperiment import WindSingleAgentExperiment
-from .abstract_experiment import CurriculumType, Learner
-
-__all__ = ['CurriculumType', 'WindSingleAgentExperiment', 'Learner']
